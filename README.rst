be5cff5960c69ea96cf94415853b39cd69ab6582
Roy Hyunjin Han <support@invisibleroads.com>
2011-08-16 14:22:52 -0400

    Rewrote everything

---
+++ README.rst
@@ -0,0 +1,12 @@
+jQuery autoComplete
+===================
+This is a rewrite of `LakTEK <http://laktek.com>`_'s `smartAutoComplete <https://github.com/laktek/jQuery-Smart-Auto-Complete>`_ plugin.
+::
+
+    $('#example1').autoComplete({
+        delay: 125,
+        maxResultCount: 5,
+        minCharacterCount: 1,
+        separator: ',',
+        sourcePacks: ['Avocado', 'Banana', 'Orange', 'Mango']
+    }).focus();
---
+++ jquery.autoComplete.js
@@ -0,0 +1,158 @@
+function filterStrings(strings, term) {
+    // Escape regular expression symbols
+    var pattern = new RegExp(term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), 'i');
+    // Filter
+    return $.grep(strings, function(x) {return pattern.test(x)});
+}
+
+$.fn.autoComplete = function(options) {
+
+    options = $.extend({}, {
+        delay: 125,
+        filter: filterStrings,
+        formatResult: function(x) {return x},
+        maxResultCount: 10,
+        minCharacterCount: 2,
+        separator: null,
+        sourcePacks: []
+    }, options);
+
+    // Make results container if it does not exist already
+    var $results = $('#autoCompleteResults');
+    if (!$results.length) $results = $('<ul>', {id: 'autoCompleteResults', style: 'display:none'}).appendTo('body');
+
+    // Define functions for extraction and completion
+    var separator = options.separator, extractTerm, replaceTerm;
+    if (separator) {
+        extractTerm = function(text) {
+            var terms = text.split(separator);
+            return $.trim(terms[terms.length - 1]);
+        };
+        replaceTerm = function(text, term) {
+            var terms = text.split(separator);
+            terms[terms.length - 1] = term;
+            return $.map(terms, $.trim).join(separator + ' ') + separator + ' ';
+        };
+    } else {
+        extractTerm = function(text) {
+            return $.trim(text);
+        };
+        replaceTerm = function(text, term) {
+            return term;
+        };
+    }
+
+    return $(this).each(function() {
+        var $input = $(this), oldCharacterCount, oldSelection;
+
+        function shiftFocus(newSelection) {
+            var $lis = $results.children(), maxSelection = $lis.length - 1;
+            if (newSelection < 0) 
+                newSelection = 0;
+            else if (newSelection > maxSelection) 
+                newSelection = maxSelection;
+            if (oldSelection != newSelection) {
+                $input.trigger('itemBlur', [$lis[oldSelection]]);
+                oldSelection = newSelection;
+            }
+            $input.trigger('itemFocus', [$lis[newSelection]]);
+        }
+
+        $input.keyup(function(e) {
+            var keyCode = e.keyCode;
+            if (keyCode == 40) {
+                // DOWN
+                if (!$results.is(':visible')) return;
+                shiftFocus(oldSelection + 1);
+            } else if (keyCode == 38) {
+                // UP
+                if (!$results.is(':visible')) return;
+                shiftFocus(oldSelection - 1);
+            } else if (keyCode == 13) {
+                // ENTER
+                if (!$results.is(':visible')) return;
+                var $lis = $results.children();
+                $input.trigger('itemSelect', [$lis[oldSelection]]);
+            } else {
+                var text = $input.val(), newCharacterCount = text.length;
+                if (oldCharacterCount == newCharacterCount) return;
+                oldCharacterCount = newCharacterCount;
+
+                if (extractTerm(text).length >= options.minCharacterCount) {
+                    setTimeout(function() {
+                        $input.trigger('autoPrompt');
+                    }, options.delay);
+                } else {
+                    $input.trigger('inputBlur');
+                }
+            }
+        }).keydown(function(e) {
+            var keyCode = e.keyCode;
+            if (keyCode == 13) {
+                // Block ENTER to prevent form submission
+                return false;
+            } else if (keyCode == 27) {
+                // Press ESC to clear autoPrompt
+                if (!$results.is(':visible')) return;
+                $input.trigger('inputBlur');
+                return false;
+            }
+        }).bind({
+            autoPrompt: function() {
+                var text = $input.val();
+                var results = options.filter(options.sourcePacks, extractTerm(text)).splice(0, options.maxResultCount);
+                if (!results.length) {
+                    $results.hide();
+                    return;
+                }
+                $results
+                    .html($.map(results, function(x) {return '<li>' + options.formatResult(x) + '</li>'}).join(''))
+                    .css({
+                        position: 'absolute',
+                        top: function() {return $input.offset().top + $input.height()},
+                        left: function() {return $input.offset().left}
+                    })
+                    .undelegate('.autoComplete')
+                    .delegate('li', {
+                        'mouseenter.autoComplete': function() {
+                            shiftFocus($(this).prevAll().length);
+                        },
+                        'mouseleave.autoComplete': function() {
+                            $input.trigger('itemBlur', [this]);
+                        },
+                        'click.autoComplete': function() {
+                            $input.trigger('itemSelect', [this]);
+                        }
+                    })
+                    .show();
+                shiftFocus(0);
+            },
+            itemFocus: function(e, li) {
+                $(li).addClass('autoCompleteSelection');
+            },
+            itemBlur: function(e, li) {
+                $(li).removeClass('autoCompleteSelection');
+            },
+            itemSelect: function(e, li) {
+                var text = replaceTerm($input.val(), $(li).text());
+                $input
+                    .val(text)
+                    .trigger('inputBlur')
+                    .focus();
+                oldCharacterCount = text.length;
+            },
+            inputBlur: function(e) {
+                $results.hide();
+            }
+        });
+
+        $(document)
+            .bind('click focusin', function(e) {
+                if (!$results.is(':visible')) return;
+                var target = e.target;
+                if (target == $results[0] || target == $input[0]) return;
+                $input.trigger('inputBlur');
+            });
+    });
+
+};
