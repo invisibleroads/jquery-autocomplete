6b66498a256497d132193d2d158c940fea83c34a
Roy Hyunjin Han <support@invisibleroads.com>
2011-09-15 11:01:06 -0400

    Added unbind() so that user can apply autoComplete() on a DOM object more than once

--- jquery.autoComplete.js
+++ jquery.autoComplete.js
@@ -1,11 +1,10 @@
-function filterStrings(strings, term) {
-    // Escape regular expression symbols
-    var pattern = new RegExp(term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), 'i');
-    // Filter
-    return $.grep(strings, function(x) {return pattern.test(x)});
-}
-
 $.fn.autoComplete = function(options) {
+
+    function filterStrings(strings, term) {
+        // Escape regular expression symbols
+        var pattern = new RegExp(term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), 'i');
+        return $.grep(strings, function(x) {return pattern.test(x)});
+    }
 
     options = $.extend({}, {
         delay: 125,
@@ -19,7 +18,8 @@ $.fn.autoComplete = function(options) {
 
     // Make results container if it does not exist already
     var $results = $('#autoCompleteResults');
-    if (!$results.length) $results = $('<ul>', {id: 'autoCompleteResults', style: 'display:none'}).appendTo('body');
+    if (!$results.length)
+        $results = $('<ul>', {id:'autoCompleteResults', style:'display:none'}).appendTo('body');
 
     // Define functions for extraction and completion
     var separator = options.separator, extractTerm, replaceTerm;
@@ -58,96 +58,101 @@ $.fn.autoComplete = function(options) {
             $input.trigger('itemFocus', [$lis[newSelection]]);
         }
 
-        $input.keyup(function(e) {
-            var keyCode = e.keyCode;
-            if (keyCode == 40) {
-                // DOWN
-                if (!$results.is(':visible')) return;
-                shiftFocus(oldSelection + 1);
-            } else if (keyCode == 38) {
-                // UP
-                if (!$results.is(':visible')) return;
-                shiftFocus(oldSelection - 1);
-            } else if (keyCode == 13) {
-                // ENTER
-                if (!$results.is(':visible')) return;
-                var $lis = $results.children();
-                $input.trigger('itemSelect', [$lis[oldSelection]]);
-            } else {
-                var text = $input.val(), newCharacterCount = text.length;
-                if (oldCharacterCount == newCharacterCount) return;
-                oldCharacterCount = newCharacterCount;
+        $input
+            .unbind('.ac')
+            .bind({
+                'keyup.ac':function(e) {
+                    var keyCode = e.keyCode;
+                    if (keyCode == 40) {
+                        // DOWN
+                        if (!$results.is(':visible')) return;
+                        shiftFocus(oldSelection + 1);
+                    } else if (keyCode == 38) {
+                        // UP
+                        if (!$results.is(':visible')) return;
+                        shiftFocus(oldSelection - 1);
+                    } else if (keyCode == 13) {
+                        // ENTER
+                        if (!$results.is(':visible')) return;
+                        var $lis = $results.children();
+                        $input.trigger('itemSelect', [$lis[oldSelection]]);
+                    } else {
+                        var text = $input.val(), newCharacterCount = text.length;
+                        if (oldCharacterCount == newCharacterCount) return;
+                        oldCharacterCount = newCharacterCount;
 
-                if (extractTerm(text).length >= options.minCharacterCount) {
-                    setTimeout(function() {
-                        $input.trigger('autoPrompt');
-                    }, options.delay);
-                } else {
-                    $input.trigger('inputBlur');
-                }
-            }
-        }).keydown(function(e) {
-            var keyCode = e.keyCode;
-            if (keyCode == 13) {
-                // Block ENTER to prevent form submission
-                return false;
-            } else if (keyCode == 27) {
-                // Press ESC to clear autoPrompt
-                if (!$results.is(':visible')) return;
-                $input.trigger('inputBlur');
-                return false;
-            }
-        }).bind({
-            autoPrompt: function() {
-                var text = $input.val();
-                var results = options.filter(options.sourcePacks, extractTerm(text)).splice(0, options.maxResultCount);
-                if (!results.length) {
-                    $results.hide();
-                    return;
-                }
-                $results
-                    .html($.map(results, function(x) {return '<li>' + options.formatResult(x) + '</li>'}).join(''))
-                    .css({
-                        position: 'absolute',
-                        top: function() {return $input.offset().top + $input.height()},
-                        left: function() {return $input.offset().left}
-                    })
-                    .undelegate('.autoComplete')
-                    .delegate('li', {
-                        'mouseenter.autoComplete': function() {
-                            shiftFocus($(this).prevAll().length);
-                        },
-                        'mouseleave.autoComplete': function() {
-                            $input.trigger('itemBlur', [this]);
-                        },
-                        'mousedown.autoComplete': function() {
-                            $input.trigger('itemSelect', [this]);
+                        if (extractTerm(text).length >= options.minCharacterCount) {
+                            setTimeout(function() {
+                                $input.trigger('autoPrompt');
+                            }, options.delay);
+                        } else {
+                            $input.trigger('inputBlur');
                         }
-                    })
-                    .show();
-                shiftFocus(0);
-            },
-            itemFocus: function(e, li) {
-                $(li).addClass('autoCompleteSelection');
-            },
-            itemBlur: function(e, li) {
-                $(li).removeClass('autoCompleteSelection');
-            },
-            itemSelect: function(e, li) {
-                var text = replaceTerm($input.val(), $(li).text());
-                $input.val(text).trigger('inputBlur');
-                setTimeout(function() {
-                    $input.focus();
-                }, 0);
-                oldCharacterCount = text.length;
-            },
-            inputBlur: function(e) {
-                $results.hide();
-            }
-        });
+                    }
+                },
+                'keydown.ac':function(e) {
+                    var keyCode = e.keyCode;
+                    if (keyCode == 13) {
+                        // Block ENTER to prevent form submission
+                        return false;
+                    } else if (keyCode == 27) {
+                        // Press ESC to clear autoPrompt
+                        if (!$results.is(':visible')) return;
+                        $input.trigger('inputBlur');
+                        return false;
+                    }
+                },
+                'autoPrompt.ac': function() {
+                    var text = $input.val();
+                    var results = options.filter(options.sourcePacks, extractTerm(text)).splice(0, options.maxResultCount);
+                    if (!results.length) {
+                        $results.hide();
+                        return;
+                    }
+                    $results
+                        .html($.map(results, function(x) {return '<li>' + options.formatResult(x) + '</li>'}).join(''))
+                        .css({
+                            position: 'absolute',
+                            top: function() {return $input.offset().top + $input.height()},
+                            left: function() {return $input.offset().left}
+                        })
+                        .undelegate('.ac')
+                        .delegate('li', {
+                            'mouseenter.ac': function() {
+                                shiftFocus($(this).prevAll().length);
+                            },
+                            'mouseleave.ac': function() {
+                                $input.trigger('itemBlur', [this]);
+                            },
+                            'mousedown.ac': function() {
+                                $input.trigger('itemSelect', [this]);
+                            }
+                        })
+                        .show();
+                    shiftFocus(0);
+                },
+                'itemFocus.ac': function(e, li) {
+                    $(li).addClass('autoCompleteSelection');
+                },
+                'itemBlur.ac': function(e, li) {
+                    $(li).removeClass('autoCompleteSelection');
+                },
+                'itemSelect.ac': function(e, li) {
+                    var text = replaceTerm($input.val(), $(li).text());
+                    $input.val(text).trigger('inputBlur');
+                    setTimeout(function() {
+                        $input.focus();
+                    }, 0);
+                    oldCharacterCount = text.length;
+                },
+                'inputBlur.ac': function(e) {
+                    $results.hide();
+                }
+            });
 
         $(document)
-            .bind('mouseup focusin', function(e) {
+            .unbind('.ac')
+            .bind('mouseup.ac focusin.ac', function(e) {
                 $input.trigger('inputBlur');
             });
     });
